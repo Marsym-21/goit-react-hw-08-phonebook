a09eab30edb3a5de48de01a8a49a93782d40174f
Marsym-21 <mini.garnett.21@gmail.com>
2023-06-20 16:57:40 +0200

    1

--- src/components/UserMenu/UserMenu.js
+++ src/components/UserMenu/UserMenu.js
@@ -1,19 +1,25 @@
 import css from './User.module.css';
 import { useSelector, useDispatch } from 'react-redux';
 import { logoutUser } from 'redux/users/usersOperations';
+import { useRef } from 'react';
+import { Link, useParams, useLocation } from 'react-router-dom';
 
 export default function UserMenu() {
   const userloggedIn = useSelector(state => state.auth.isLoggedIn);
   console.log(userloggedIn);
   const userName = useSelector(state => state.auth.user.name);
   const dispatch = useDispatch();
+  const location = useLocation();
+  const backLinkLocationRef = useRef(location.state?.from ?? '/');
 
   return userloggedIn ? (
     <div className={css.nav}>
       <p className={css.text}>{userName}</p>
-      <button className={css.btn} onClick={() => dispatch(logoutUser())}>
-        Logout
-      </button>
+      <Link to={backLinkLocationRef.current}>
+        <button className={css.btn} onClick={() => dispatch(logoutUser())}>
+          Logout
+        </button>
+      </Link>
     </div>
   ) : (
     <div className={css.nav}>
