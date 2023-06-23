f85ca3b37fe2fc491053bfded7774a01c60637b0
Marsym-21 <mini.garnett.21@gmail.com>
2023-06-23 05:43:59 +0200

    1

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -1,32 +1,20 @@
-// import { useMemo } from 'react';
+import { useMemo, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-// import { useEffect } from 'react';
 import {
   deleteContacts,
   fetchContacts,
 } from 'redux/contacts/contactsOperations';
 import css from './Contacts.module.css';
-import { useEffect } from 'react';
 
 const Contacts = () => {
-  // const filterValue = useSelector(state => state.valueFilter);
+  const filterValue = useSelector(state => state.valueFilter);
   const contactsValue = useSelector(state => state.contacts.entities);
   const deleteContactTrue = useSelector(
     state => state.contacts.deleteContactFulfilled
   );
   const dispatch = useDispatch();
   console.log(contactsValue);
-
-  // const visibleContacts = () => {
-  //   const normalizeFilter = filterValue.toLowerCase();
-  //   if (contactsValue.length > 0) {
-  //     return contactsValue.filter(({ name }) =>
-  //       name.toLowerCase().includes(normalizeFilter)
-  //     );
-  //   } else {
-  //     return;
-  //   }
-  // };
+  console.log(filterValue);
 
   useEffect(() => {
     if (deleteContactTrue) {
@@ -34,10 +22,17 @@ const Contacts = () => {
     }
   }, [dispatch, deleteContactTrue]);
 
+  const visibleContacts = useMemo(() => {
+    const normalizeFilter = filterValue.toLowerCase();
+    return contactsValue.filter(({ name }) =>
+      name.toLowerCase().includes(normalizeFilter)
+    );
+  }, [contactsValue, filterValue]);
+
   return (
     <ul className={css.list}>
       {contactsValue.length > 0 ? (
-        contactsValue.map(({ id, name, phone }) => (
+        visibleContacts.map(({ id, name, phone }) => (
           <li className={css.item} key={id}>
             &#10003; {name}: {phone}
             <button
